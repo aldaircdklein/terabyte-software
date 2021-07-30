d79d632bf14075cf1c7784cde126efef611190db
aldaircdklein <camargoterabyte@gmail.com>
2021-07-30 20:27:43 -0300

    Correct request venda

--- frontend/src/app/pages/home/components/lista_venda/service.js
+++ frontend/src/app/pages/home/components/lista_venda/service.js
@@ -8,13 +8,17 @@ export const ListaVendas = async (dados) => {
         if(dados.dateStart && dados.dateEnd){
             result = await api.get(ServeRoutes(`startDate=${dados.dateStart}&endDate=${dados.dateEnd}${'&paid='+dados.paid}`).listServiceOrderUnpaid);
         }else{
-            result = await api.get(ServeRoutes(`startDate=2019-01-01&endDate=${format(new Date(), 'yyyy-MM-dd')}${'&paid='+dados.paid}`).listServiceOrderUnpaid);
+            let data = new Date();
+            data.setDate(data.getDate() + 2);
+            result = await api.get(ServeRoutes(`startDate=2019-01-01&endDate=${format(data, 'yyyy-MM-dd')}${'&paid='+dados.paid}`).listServiceOrderUnpaid);
         }
     }else{
         if(dados.dateStart && dados.dateEnd){
             result = await api.get(ServeRoutes(`startDate=${dados.dateStart}&endDate=${dados.dateEnd}${dados.name !== ''? '&name='+dados.name:''}${'&paid='+dados.paid}`).listSold);
         }else{
-            result = await api.get(ServeRoutes(`startDate=2019-01-01&endDate=${format(new Date(), 'yyyy-MM-dd')}${dados.name !== ''? '&name='+dados.name:''}${'&paid='+dados.paid}`).listSold);
+            let data = new Date();
+            data.setDate(data.getDate() + 2);
+            result = await api.get(ServeRoutes(`startDate=2019-01-01&endDate=${format(data, 'yyyy-MM-dd')}${dados.name !== ''? '&name='+dados.name:''}${'&paid='+dados.paid}`).listSold);
         }
     }
     return result.data;
